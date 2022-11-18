d28dcc72e157a55297bde06ca8784ba98bff5b9b
AlexAbreu56 <aabreu56@ladueschools.net>
2022-11-18 17:25:11 +0000

    hehehehhe

--- ConsumerLab_Code/Review.java
+++ ConsumerLab_Code/Review.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
+
+import javax.management.StandardEmitterMBean;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Random;
 import java.io.*;
+import java.util.Arrays;
 
 /**
  * Class that contains helper methods for the Review Lab
  **/
 public class Review {
-  
+  public static void main(String[] args){
+    System.out.println(starRating("/workspace/APCS/ConsumerLab_Code/SimpleReview.txt"));
+    System.out.println(totalSentiment("/workspace/APCS/ConsumerLab_Code/SimpleReview.txt"));
+    System.out.println(fakeReview("/workspace/APCS/ConsumerLab_Code/SimpleReview.txt"));
+  }
   private static HashMap<String, Double> sentiment = new HashMap<String, Double>();
   private static ArrayList<String> posAdjectives = new ArrayList<String>();
   private static ArrayList<String> negAdjectives = new ArrayList<String>();
  
   
   private static final String SPACE = " ";
-  
   static{
     try {
-      Scanner input = new Scanner(new File("cleanSentiment.csv"));
+      Scanner input = new Scanner(new File("/workspace/APCS/ConsumerLab_Code/cleanSentiment.csv"));
       while(input.hasNextLine()){
         String[] temp = input.nextLine().split(",");
         sentiment.put(temp[0],Double.parseDouble(temp[1]));
@@ -34,7 +41,7 @@ public class Review {
   
   //read in the positive adjectives in postiveAdjectives.txt
      try {
-      Scanner input = new Scanner(new File("positiveAdjectives.txt"));
+      Scanner input = new Scanner(new File("/workspace/APCS/ConsumerLab_Code/positiveAdjectives.txt"));
       while(input.hasNextLine()){
         String temp = input.nextLine().trim();
         System.out.println(temp);
@@ -48,7 +55,7 @@ public class Review {
  
   //read in the negative adjectives in negativeAdjectives.txt
      try {
-      Scanner input = new Scanner(new File("negativeAdjectives.txt"));
+      Scanner input = new Scanner(new File("/workspace/APCS/ConsumerLab_Code/negativeAdjectives.txt"));
       while(input.hasNextLine()){
         negAdjectives.add(input.nextLine().trim());
       }
@@ -162,4 +169,64 @@ public class Review {
       return randomNegativeAdj();
     }
   }
+  public static double totalSentiment( String filename )
+  {
+    double sum = 0;
+    String x = Review.textToString(filename);
+    String [] y = x.split(" ");
+    ArrayList<String> strList = new ArrayList<String>(Arrays.asList(y));
+    for(int i = 0; i < strList.size(); i ++){
+      sum += sentimentVal(strList.get(i));
+    }
+    return(sum);
+  }
+  public static int starRating(String filename)
+  {
+    int star = 0;
+    double sVal = totalSentiment(filename);
+    if(sVal >= 10)
+    {
+      star = 5;
+    }
+    else if(sVal >= 8)
+    {
+      star = 4;
+    }
+    else if(sVal >= 6)
+    {
+      star = 3;
+    }
+    else if(sVal >=4)
+    {
+      star = 2;
+    }
+    else if(sVal >=2)
+    {
+      star = 1;
+    }
+    else
+    {
+      star = 1;
+    }
+    return (star);
+  }
+  public static String fakeReview(String filename)
+  {
+    String x = textToString(filename);
+    String [] y = x.split(" ");
+    String ns = "";
+    ArrayList<String> strList = new ArrayList<String>(Arrays.asList(y));
+    for(int i = 0; i < strList.size(); i ++){
+      if(strList.get(i).contains("*"))
+      {
+        ns+=randomAdjective() + " ";
+      }
+      else
+      {
+        ns += strList.get(i) + " ";
+      }
+    }
+    return(ns);
+  }
 }
+
---
+++ PhraseSolver/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args)
+    {
+        Board NewBoard = new Board();
+
+        System.out.println(NewBoard.GuessPhrase());
+    }
+}
